d14d689608d99b8e7a7d16cbe212c52f522df9b9
dylan <2857015121@qq.com>
2026-08-03 14:52:53 +0800

    项目框架

---
+++ src/main/java/com/dylan/personalhub/PersonalhubApplication.java
@@ -0,0 +1,13 @@
+package com.dylan.personalhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalhubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalhubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dylan/personalhub/PersonalhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dylan.personalhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalhubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
