797ecdf9e12197019f5028d07425b95348d86ecf
dylan <2857015121@qq.com>
2026-08-09 16:20:59 +0800

    后台编辑回退功能

--- src/main/java/com/dylan/personalhub/controller/admin/FileUploadController.java
+++ src/main/java/com/dylan/personalhub/controller/admin/FileUploadController.java
@@ -1,6 +1,8 @@
 package com.dylan.personalhub.controller.admin;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -13,19 +15,23 @@ import java.util.UUID;
 @RequestMapping("/admin/upload")
 public class FileUploadController {
 
+    private static final Logger log = LoggerFactory.getLogger(FileUploadController.class);
+
     private static final String UPLOAD_PATH = System.getProperty("user.dir") + "/uploads/images/";
 
     @PostMapping("/image")
-
-
     public String upload(@RequestParam("file") MultipartFile file)
             throws IOException {
+
+        log.info("收到上传请求: fileName={}, size={} bytes",
+                file.getOriginalFilename(), file.getSize());
 
         // 创建目录
         File directory = new File(UPLOAD_PATH);
 
         if (!directory.exists()) {
             directory.mkdirs();
+            log.info("创建上传目录: {}", directory.getAbsolutePath());
         }
 
         // 文件名
@@ -37,7 +43,10 @@ public class FileUploadController {
         // 保存文件
         file.transferTo(target);
 
-        // 返回 Markdown 图片地址
+        log.info("文件上传成功: {} -> {}", file.getOriginalFilename(),
+                target.getAbsolutePath());
+
+        // 返回图片访问地址
         return "/uploads/images/" + filename;
 
     }
